86d3eb0ea8b31d53cdfff1f2d78f7a136f681fb1
S4v!0r <gbp17@outlook.com>
2024-08-14 22:34:00 -0500

    feat: Update backend dependencies and add socket.io to package.json

--- backend/src/messages/messages.gateway.ts
+++ backend/src/messages/messages.gateway.ts
@@ -1,7 +1,22 @@
-import { WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  WebSocketGateway,
+} from '@nestjs/websockets';
+import { Socket } from 'socket.io';
 import { MessagesService } from './messages.service';
 
 @WebSocketGateway({ cors: true })
-export class MessagesGateway {
+export class MessagesGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
   constructor(private readonly messagesService: MessagesService) {}
+
+  handleConnection(client: Socket) {
+    this.messagesService.registerClient(client);
+  }
+
+  handleDisconnect(client: Socket) {
+    this.messagesService.unregisterClient(client.id);
+  }
 }
